88618dbf7d5c6394591ba474a13522b5d3f81783
charles edange <charlesedange@hotmail.com>
2017-11-22 17:00:25 +0100

    mvp ok

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '01 02 02 03 04'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '01 58 76 87 90'
+  },
+  {
+    name:         'L entrecôte',
+    address:      '140 boulevard de montparnasse 75006 Paris',
+    category:     'french',
+    phone_number: '01 58 76 87 90'
+  },
+  {
+    name:         'La belle frite',
+    address:      '8 rue du colisée',
+    category:     'belgian',
+    phone_number: '01 32 45 87 22'
+  },
+  {
+    name:         'L alicheur',
+    address:      '10 rue saint maur',
+    category:     'chinese',
+    phone_number: '01 57 77 40 90'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
